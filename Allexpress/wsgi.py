b2b041f8090a5c1990f406f35703c8407ed8da54
Sohel-Rana5510 <sohelranashowrov@gmail.com>
2026-05-11 01:01:56 +0600

    Add automatic superuser creation logic

--- Allexpress/wsgi.py
+++ Allexpress/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Allexpress.settings')
 
 application = get_wsgi_application()
+
+# Automatic Superuser Creation
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('SUPERUSER_USERNAME')
+email = os.environ.get('SUPERUSER_EMAIL')
+password = os.environ.get('SUPERUSER_PASSWORD')
+
+if username and password and email:
+    if not User.objects.filter(username=username).exists():
+        print(f"Creating superuser {username}...")
+        User.objects.create_superuser(username=username, email=email, password=password)
+    else:
+        print(f"Superuser {username} already exists.")
+
